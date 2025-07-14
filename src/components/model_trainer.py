33e54dfd2fccba054c2a18827fe06a32d3ed487c
klan86at <klang86at@gmail.com>
2025-07-14 09:30:10 +0300

    Hyperparaeter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,17 +42,98 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "K-Neighbors": KNeighborsRegressor(),
                 "XGBClassifier": KNeighborsRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
                 "Ridge": Ridge(),
                 "Lasso": Lasso()
             }
+            param_grid = {
+
+            "Decision Tree": {
+                'max_depth': [3, 5, None],
+                'min_samples_split': [2, 5],
+                'min_samples_leaf': [1, 2],
+                'criterion': ['squared_error']
+            },
+
+            "Random Forest": {
+                'n_estimators': [100],  # Reduced from higher values
+                'max_depth': [5, 10, None],
+                'min_samples_split': [2, 4],
+                'min_samples_leaf': [1, 2],
+                'max_features': ['sqrt'],
+                'bootstrap': [True]
+            },
+
+            "Linear Regression": {},
+
+            "Gradient Boosting": {
+                'n_estimators': [100],
+                'learning_rate': [0.1],
+                'max_depth': [3, 5],
+                'min_samples_split': [2],
+                'min_samples_leaf': [1],
+                'subsample': [0.8]
+            },
+
+            "K-Neighbors": {
+                'n_neighbors': [3, 5],
+                'weights': ['uniform', 'distance'],
+                'p': [2]  # Euclidean distance only
+            },
+
+            "XGBoost": {
+                'n_estimators': [100],
+                'learning_rate': [0.1],
+                'max_depth': [3, 5],
+                'min_child_weight': [1, 3],
+                'subsample': [0.8],
+                'colsample_bytree': [0.8],
+                'gamma': [0],
+                'reg_alpha': [0],
+                'reg_lambda': [1],
+                'eval_metric': ['rmse']
+            },
+
+            "CatBoost": {
+                'iterations': [100],
+                'learning_rate': [0.1],
+                'depth': [4, 6],
+                'border_count': [32, 128],
+                'l2_leaf_reg': [1, 3],
+                'random_strength': [1]
+            },
+
+            "AdaBoost": {
+                'n_estimators': [50],
+                'learning_rate': [0.1, 1.0],
+                'loss': ['linear'],
+                'base_estimator__max_depth': [2, 3]
+            },
+
+            "Ridge": {
+                'alpha': [0.1, 1.0],
+                'solver': ['auto'],
+                'max_iter': [1000],
+                'tol': [1e-3]
+            },
+
+            "Lasso": {
+                'alpha': [0.01, 0.1],
+                'selection': ['cyclic'],
+                'max_iter': [1000],
+                'tol': [1e-3]
+            }
+        }
+
+        
             model_report:dict=evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=param_grid
             )
 
             # To get best model score from dict
@@ -78,7 +159,7 @@ class ModelTrainer:
 
             r_square = r2_score(y_test, predicted)
 
-            return r_square
+            return f"{r_square:.4f}"
         
 
         except Exception as e:
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -20,14 +21,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range (len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+
+            search = RandomizedSearchCV(model, param_distributions=param, n_iter=30, cv=3, n_jobs=2)
+            search.fit(X_train, y_train)
+
+            model.set_params(**search.best_params_)
             model.fit(X_train, y_train)
+            
+            # model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
 
